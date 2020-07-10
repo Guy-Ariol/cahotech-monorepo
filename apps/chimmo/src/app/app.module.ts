efe60fe1a95576367106cab1284a00e1fd39abdf
MBAFA <software@mbafa.cm>
2020-07-10 22:50:24 +0200

    chimmo app pages and services created.

--- apps/chimmo/src/app/app.module.ts
+++ apps/chimmo/src/app/app.module.ts
@@ -10,12 +10,29 @@ import { fr_FR } from 'ng-zorro-antd/i18n';
 import { registerLocaleData } from '@angular/common';
 import fr from '@angular/common/locales/fr';
 
+import { AdminComponent } from './pages/admin/admin.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LandlordComponent } from './pages/landlord/landlord.component';
+import { RenterComponent } from './pages/renter/renter.component';
+
 registerLocaleData(fr);
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
+  declarations: [
+    AppComponent,
+    AdminComponent,
+    HomeComponent,
+    LandlordComponent,
+    RenterComponent],
+
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule],
+
   providers: [{ provide: NZ_I18N, useValue: fr_FR }],
+
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
---
+++ apps/chimmo/src/app/pages/landlord/landlord.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cahotech-monorepo-landlord',
+  templateUrl: './landlord.component.html',
+  styleUrls: ['./landlord.component.scss']
+})
+export class LandlordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
