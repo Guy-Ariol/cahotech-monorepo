a98957c92abf8c0c4c8d573e2fa1a2ea779be9ca
MBAFA <software@mbafa.cm>
2020-07-10 22:41:59 +0200

    ng-zorro installed. fixed import issue in .less file

--- apps/chimmo/src/app/app.module.ts
+++ apps/chimmo/src/app/app.module.ts
@@ -2,11 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { fr_FR } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import fr from '@angular/common/locales/fr';
+
+registerLocaleData(fr);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, FormsModule, HttpClientModule, BrowserAnimationsModule],
+  providers: [{ provide: NZ_I18N, useValue: fr_FR }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
