2a56d6abe85f03ef9b9689878f3e6652f5298413
MBAFA <software@mbafa.cm>
2020-07-13 21:47:44 +0200

    routing and ng-zorro activated 🏆🏆

--- apps/napata-home/src/app/app.module.ts
+++ apps/napata-home/src/app/app.module.ts
@@ -2,11 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
+    AppRoutingModule,
+    NzButtonModule
 
   ],
   providers: [],
