ff1e4e6cdc85a6beab4980f736e74594c6e6df93
MBAFA <software@mbafa.cm>
2020-08-22 16:12:55 +0200

    griot-moderne app created

---
+++ apps/griot-moderne/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'griot-moderne',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/griot-moderne',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
