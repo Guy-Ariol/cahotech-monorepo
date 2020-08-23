3def2ebeb3611247e0e3a30318a9cdbc7ed495ec
MBAFA <software@mbafa.cm>
2020-08-23 04:47:03 +0200

    app design started

--- apps/griot-moderne/src/app/app.component.ts
+++ apps/griot-moderne/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { UtilsService } from 'libs/service/src/lib/utils/utils.service';
 
 @Component({
   selector: 'cahotech-monorepo-root',
@@ -6,5 +7,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'griot-moderne';
+  features = [
+    {
+      title: 'Gagner en temps',
+      pic: 'assets/imgs/read-fast.png'
+    },
+    {
+      title: 'Emporter vous livres partout',
+      pic: 'assets/imgs/read-fast.png'
+    },
+    {
+      title: 'Emporter vous livres partout',
+      pic: 'assets/imgs/read-fast.png'
+    }
+  ]
+
+  constructor(
+    public utilsProv: UtilsService
+  ) {
+
+  }
+
+  ngOnInit () {
+    this.utilsProv.setScreenSize(window.innerWidth, window.innerHeight)
+  }
+
+  @HostListener('window:resize', ['$event'])
+  private onResize (event) {
+    this.utilsProv.setScreenSize(window.innerWidth, window.innerHeight)
+  }
 }
+
+
--- apps/griot-moderne/src/app/app.module.ts
+++ apps/griot-moderne/src/app/app.module.ts
@@ -2,11 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventEmitter } from 'events';
+import { ToastrModule } from 'ngx-toastr';
+
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [
+    BrowserModule,
+    ToastrModule.forRoot(),
+
+  ],
+  providers: [EventEmitter],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
