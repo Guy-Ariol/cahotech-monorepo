b6b716c799720a6cc9a76883d3be2eeb879f4c29
MBAFA <software@mbafa.cm>
2020-08-30 21:40:20 +0200

    app pages created

---
+++ apps/chimmo/src/app/pages/release/release.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cahotech-monorepo-release',
+  templateUrl: './release.component.html',
+  styleUrls: ['./release.component.scss']
+})
+export class ReleaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
